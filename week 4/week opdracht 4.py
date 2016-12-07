8bbd511340539b775db031e3f089a773fb9c9656
Alexanderkorn <alexanderkorn7@gmail.com>
2016-12-07 14:11:45 +0100

    new

---
+++ week 4/week opdracht 4.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as etree
+
+tree = etree.parse('./week_opdracht_4.xml')
+root = tree.getroot()
+#print("title van de server 1 is:", root.find('servers/server[@id="1"]/title').text)
+#print("title van de server 1 is:", root.find('servers/server[@id="2"]/title').text)
+
+servers = root.findall('.//server')
+clients = root.findall('.//client')
+for server in servers:
+    print("\nServer met id:", server.attrib['id'])
+    print("Server naam is:", server.find('title').text)
+    print("Server ipaddress is:", server.find('ipaddress').text)
+    print("Server harddisks:", server.find('harddisks_1').text)
+    print("Server harddisks:", server.find('harddisks_2').text)
+    print("Server harddisks:", server.find('harddisks_3').text)
+    print("Server OS:", server.find('OS').text)
+
+for client in clients:
+    print("\nServer met id:", client.attrib['id'])
+    print("Server naam is:", client.find('title').text)
+    print("Server ipaddress is:", client.find('ipaddress').text)
+    print("Server harddisks:", client.find('harddisks_1').text)
+    print("Server OS:", client.find('OS').text)
