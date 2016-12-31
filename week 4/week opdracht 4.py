586fe36a6bebad88eb077db009e3eb909f820563
Alexanderkorn <alexanderkorn7@gmail.com>
2016-12-31 10:47:47 +0100

    imporvements on code

--- week 4/week opdracht 4.py
+++ week 4/week opdracht 4.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as etree
 
-tree = etree.parse('./week_opdracht_4.xml')
+tree = etree.parse('C//users//HP-Alexander//week_opdracht_4.xml')
 root = tree.getroot()
 #print("title van de server 1 is:", root.find('servers/server[@id="1"]/title').text)
 #print("title van de server 1 is:", root.find('servers/server[@id="2"]/title').text)
@@ -14,12 +14,13 @@ for server in servers:
     print("Server harddisks:", server.find('harddisks_1').text)
     print("Server harddisks:", server.find('harddisks_2').text)
     print("Server harddisks:", server.find('harddisks_3').text)
-    print("Server OS:", server.find('OS').text)
+    print("free space on disk D:", server.find('freespace_D').text)
+    #print("Server OS:", server.find('OS').text)
 
-for client in clients:
-    print("\nServer met id:", client.attrib['id'])
-    print("Server naam is:", client.find('title').text)
-    print("Server ipaddress is:", client.find('ipaddress').text)
-    print("Server harddisks:", client.find('harddisks_1').text)
-    print("Server OS:", client.find('OS').text)
-
+# for client in clients:
+#     print("\nServer met id:", client.attrib['id'])
+#     print("Server naam is:", client.find('title').text)
+#     print("Server ipaddress is:", client.find('ipaddress').text)
+#     print("Server harddisks:", client.find('harddisks_1').text)
+#     print("Server OS:", client.find('OS').text)
+#
